8a478a89c20bacfda2a90ced30f2f24b646fd0e3
Ngoc Duc Nghiem <161023401+NghiemNgocDuc@users.noreply.github.com>
2026-03-12 10:57:12 -0400

    Refactor cache creation and management functions

--- project/src/cache.c
+++ project/src/cache.c
@@ -4,10 +4,12 @@
 #include <stdlib.h>
 #include "cpu.h"
 #include "lru.h"
+
 char *make_block(int block_size) {
   char *block = (char *)calloc(block_size, sizeof(char));
   return block;
 }
+
 Line *make_lines(int line_count, int block_size) {
   Line *duc = (Line *)calloc(line_count, sizeof(Line));
   for (int i = 0; i < line_count; i++) {
@@ -18,6 +20,7 @@ Line *make_lines(int line_count, int block_size) {
   }
   return duc;
 }
+
 Set *make_sets(int set_count, int line_count, int block_size) {
   Set *hanh = (Set *)calloc(set_count, sizeof(Set));
   for (int i = 0; i < set_count; i++) {
@@ -26,35 +29,44 @@ Set *make_sets(int set_count, int line_count, int block_size) {
   }
   return hanh;
 }
-Cache *make_cache(int set_count, int line_count, int block_size) {
+
+Cache *make_cache(int set_bits, int line_count, int block_bits) {
   Cache *cache = (Cache *)malloc(sizeof(Cache));
-  cache->sets = make_sets(set_count, line_count, block_size);
-  cache->set_count = set_count;
+  cache->set_bits = set_bits;
+  cache->block_bits = block_bits;
+  cache->set_count = (int)exp2(set_bits);
+  cache->block_size = (int)exp2(block_bits);
   cache->line_count = line_count;
-  cache->block_size = block_size;
-  cache->block_bits = (int)log2(block_size);
-  cache->set_bits = (int)log2(set_count);
-  lru_init(cache);
-  return cache;
+  cache->sets = (Set *)make_sets(cache->set_count, line_count, cache->block_size);
+  // Create LRU queues for sets:
+  if (cache != NULL) {
+    lru_init(cache);
+  }
+  return cache;  
 }
+
 void delete_block(char *accessed) { free(accessed); }
+
 void delete_lines(Line *lines, int line_count) {
   for (int i = 0; i < line_count; i++) {
     delete_block(lines[i].block);
   }
   free(lines);
 }
+
 void delete_sets(Set *sets, int set_count) {
   for (int i = 0; i < set_count; i++) {
     delete_lines(sets[i].lines, sets[i].line_count);
   }
   free(sets);
 }
+
 void delete_cache(Cache *cache) {
   lru_destroy(cache);
   delete_sets(cache->sets, cache->set_count);
   free(cache);
 }
+
 SearchInfo get_bits(Cache *cache, address_type address) {
   SearchInfo result;
   result.offset = address & (cache->block_size - 1);
@@ -62,24 +74,30 @@ SearchInfo get_bits(Cache *cache, address_type address) {
   result.tag = address >> (cache->block_bits + cache->set_bits);
   return result;
 }
+
 AccessResult cache_access(Cache *cache, TraceLine *trace_line) {
   SearchInfo bits = get_bits(cache, trace_line->address);
   unsigned int s = bits.set_id;
   unsigned int t = bits.tag;
   unsigned int b = bits.offset;
+
   // Get the set:
   Set *set = &cache->sets[s];
+
   // Get the line:
   LRUResult result;
   lru_fetch(set, t, &result);
   Line *line = result.line;
+
   // If it was a miss we will clear the accessed bits:
   if (result.access != HIT) {
     for (int i = 0; i < cache->block_size; i++) {
       line->block[i] = 0;
     }
   }
+
   // Then set the accessed byte to 1:
   line->block[b] = 1;
+
   return result.access;
 }
