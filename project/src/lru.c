bbb0169a67fbec321dc770e39a197fe7b3f632fc
Ngoc Duc Nghiem <161023401+NghiemNgocDuc@users.noreply.github.com>
2026-03-12 11:04:51 -0400

    Implement LRU algorithm in lru_fetch function

--- project/src/lru.c
+++ project/src/lru.c
@@ -39,8 +39,50 @@ void lru_destroy(Cache *cache) {
 }
 
 void lru_fetch(Set *set, unsigned int tag, LRUResult *result) {
-  // TODO:
-  // Implement the LRU algorithm to determine which line in
-  // the cache should be accessed.
-  //
+  LRUNode *curr = set->lru_queue;
+  LRUNode *prev = NULL;
+  LRUNode *target_node = NULL;
+  LRUNode *target_prev = NULL;
+
+  
+  while (curr != NULL) {
+    if (curr->line->valid && curr->line->tag == tag) {
+      //  HIT
+      result->access = HIT;
+      target_node = curr;
+      target_prev = prev;
+      break;
+    } else if (!curr->line->valid && target_node == NULL) {
+      // COLD_MISS 
+      result->access = COLD_MISS;
+      target_node = curr;
+      target_prev = prev;
+    }
+    prev = curr;
+    curr = curr->next;
+  }
+
+  // CONFLICT_MISS 
+  if (result->access != HIT && target_node == NULL) {
+    result->access = CONFLICT_MISS;
+    target_node = prev;      
+    target_prev = NULL;       
+    LRUNode *p = set->lru_queue;
+    target_prev = NULL;
+    while (p->next != target_node) {
+      target_prev = p;
+      p = p->next;
+    }
+    target_prev = p;
+  }
+
+  
+  target_node->line->tag = tag;
+  target_node->line->valid = 1;
+  result->line = target_node->line;
+  if (target_prev != NULL) {
+    target_prev->next = target_node->next;
+    target_node->next = set->lru_queue;
+    set->lru_queue = target_node;
+  }
 }
